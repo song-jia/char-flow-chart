bc98df7bd420caf411856ce9c4e1c6b1f1aa3588
Song, Jia <jsong@nltechdev.com>
2016-01-07 09:28:12 +0800

    add react-hot-loader plugin

--- webpack.config.js
+++ webpack.config.js
@@ -6,18 +6,21 @@ var webpack = require('webpack'),
 module.exports = {
   target: 'web',
   cache: true,
-  context: path.join(__dirname, 'src'),
   entry: [
-    './index.jsx'
+    'webpack-dev-server/client?http://localhost:3000', // WebpackDevServer host and port
+    'webpack/hot/only-dev-server', // "only" prevents reload on syntax errors
+    './src/index.jsx'
   ],
   output: {
     path: path.join(__dirname, 'dist'),
     filename: '[name].js',
     chunkFilename: 'modules/[name].[chunkhash].js',
-    sourceMapFilename: '[file].map'
+    sourceMapFilename: '[file].map',
   },
   module: {
     loaders: [
+      // react hot loader
+      { test: /\.jsx?$/, loader: 'react-hot', include: path.join(__dirname, 'src') },
       // babel-loader
       {
         test: /\.jsx?$/,
@@ -30,7 +33,10 @@ module.exports = {
     ]
   },
   plugins: [
-    new webpack.optimize.CommonsChunkPlugin('common', 'common.js')
+    new webpack.optimize.CommonsChunkPlugin('common', 'common.js'),
+    // HotModuleReplacementPlugin only be needed when start webpack-dev-server from API, it need to be removed
+    // when start server from CLI with --hot option.
+    new webpack.HotModuleReplacementPlugin()
   ],
   debug: true,
   devtool: '#source-map',
